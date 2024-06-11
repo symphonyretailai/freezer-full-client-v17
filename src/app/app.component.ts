ebbbb1e83e8e0144910af49410b45bbd75fb0aef
Barry Nicola <Barry.Nicola@symphonyretailai.com>
2024-06-11 10:01:01 +0100

    fully converted to standalone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 ﻿import { Component } from '@angular/core';
+import { AlertComponent } from './_components/alert.component';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
-@Component({ selector: 'app-root', templateUrl: 'app.component.html' })
+@Component({
+    selector: 'app-root', templateUrl: 'app.component.html',
+    standalone: true,
+    imports: [RouterLink, RouterLinkActive, AlertComponent, RouterOutlet]
+})
 export class AppComponent { }
